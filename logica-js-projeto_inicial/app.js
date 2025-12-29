4280f1314ea5afe8da61dd79b5a58e1cf974e80b
Bia Pinheiro <anabeatrizdp2@gmail.com>
2025-12-29 19:43:40 -0300

    Organização e exercicio Function

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,4 +1,7 @@
-//1.Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+//--------------------------------------------------------- ALERT, CONSOLE, TEMPALTE STRING, PROMPT ----------------------------------------------------------------------------
+
+
+/*//1.Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
 alert('Boas vindas ao nosso site');
 
 //2.Declare uma variável chamada nome e atribua a ela o valor "Lua".
@@ -29,6 +32,8 @@ let nameUser = prompt('Como você se chama?');
 
 //9.Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
 let ageUser = prompt('Qual é a sua idade?');
+
+//--------------------------------------------------------- IF-ELSE ----------------------------------------------------------------------------
 
 //10.Agora, para validar a idade que capturamos no desafio 09, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
 if(ageUser >= 18){
@@ -77,6 +82,8 @@ alert(`Seu saldo na conta é: ${saldoConta}`);
 let nomeUser = prompt('What is your name?');
 alert(`Welcome ${nomeUser}`);
 
+//--------------------------------------------------------- WHILE ----------------------------------------------------------------------------
+
 //16.Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
 let contador = 1;
 
@@ -109,6 +116,8 @@ while (contador3 <= contagem2) {
     alert(contador3);
     contador3++;
 }
+
+//--------------------------------------------------------- EXTRA ----------------------------------------------------------------------------
 
 //20.Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
 let welcomeMessage = 'Seja bem vindos(a)!';
@@ -165,6 +174,8 @@ if (nota >= 7) {
     console.log('Reprovado');
 }
 
+//--------------------------------------------------------- MATH.RANDOM ----------------------------------------------------------------------------
+
 //30.Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
 let numeroAleatorio = Math.random();
 console.log(numeroAleatorio);
@@ -176,3 +187,140 @@ console.log(numeroAleatorio1);
 //32.Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
 let numeroAleatorio2 = parseInt(Math.random() * 1000 + 1);
 console.log(numeroAleatorio2);
+*/
+//--------------------------------------------------------- FUNÇÃO ----------------------------------------------------------------------------------
+
+//32.Criar uma função que exibe "Olá, mundo!" no console.
+function exibirOlaMundo() {
+    console.log('Olá, mundo!');
+}
+
+exibirNome();
+
+//33.Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function exibirNome(name1) {
+    alert(`Olá, ${name1}`);
+}
+
+exibirNome('Ana');
+
+//34.Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function numeroDobro(numero) {
+    return numero * 2 ;
+}
+
+let resultadoDobro = numeroDobro(5);
+console.log(resultadoDobro);
+
+//35.Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function mediaNumeros(number1, number2, number3) {
+    return (number1 + number2 + number3)/3;
+}
+
+let media2 = mediaNumeros(3,7,2);
+console.log(media2);
+
+//36.Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maiorDosDoisNumeros(number1, number2) {
+    return number1 > number2 ? number1 : number2
+}
+
+let doisNumeros = maiorDosDoisNumeros(8,3);
+console.log(doisNumeros);
+
+//37.Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+function multiplicadoPorEleMesmo(number) {
+    return number * number;
+}
+
+let numeroSi = multiplicadoPorEleMesmo(5);
+console.log(numeroSi);
+
+// 38.Criar uma função que recebe um valor em metros e retorna o valor convertido em centímetros (Dica: 1 metro = 100 centímetros).
+function conversorDeMedidas(valor) {
+    return valor * 100;
+}
+
+let resultadoConvertido = conversorDeMedidas(4);
+console.log(resultadoConvertido);
+
+//39. Criar uma função que recebe um número e retorna a string "Par" se o número for par, ou "Ímpar" se for ímpar (Dica: use o operador de resto %).
+function verificarParOuImpar(numero) {
+     if (numero % 2 == 0) {
+        return 'Par';
+    } else {
+        return 'Impar';
+    } 
+}
+
+let resultadoParOuImpar = verificarParOuImpar(5);
+console.log(resultadoParOuImpar);
+
+//40. Criar uma função que recebe a base e a altura de um retângulo e retorna a sua área ($Area = base \times altura$).
+function areaRetangulo(base, altura) {
+    return base * altura;
+}
+
+let resultadoAreaRetangulo = areaRetangulo(6,3);
+console.log(resultadoAreaRetangulo);
+
+//41. Criar uma função que recebe a idade de uma pessoa e retorna true se ela for maior de idade (18 anos ou mais) e false caso contrário.
+function idadeMaiorOuMenor(idade) {
+    console.log(idade >= 18);    
+}
+
+let resultadoIdade = idadeMaiorOuMenor(6);
+
+//42. Criar uma função que recebe a hora atual (apenas o número inteiro, de 0 a 23) e exibe "Bom dia", "Boa tarde" ou "Boa noite" no console.
+function saudacaoDia(hora) {
+    if (hora < 12) {
+        return 'Bom dia';
+    } else if (hora > 12) {
+        return 'Boa tarde';
+    } else {
+        return 'Boa noite';
+    }
+}
+
+let resultadoSaudacao  = saudacaoDia(16);
+console.log(resultadoSaudacao);
+
+//43. Criar uma função que recebe um número e retorna uma string dizendo se o número é "Positivo", "Negativo" ou "Zero".
+function verificarNumero(numero) {
+    if (numero > 0) {
+        return 'Positivo'
+    } else if (numero < 0) {
+        return 'Negativo'
+    } else {
+        return 'Zero'
+    }
+}
+
+let resultadoNumero2 = verificarNumero(9);
+console.log (resultadoNumero2);
+
+//44.Crie uma função que recebe o peso e a altura. Ela deve calcular o IMC (IMC = peso / (altura * altura)) e retornar uma string:Se o IMC for menor que 18.5: "Abaixo do peso"Se estiver entre 18.5 e 24.9: "Peso normal"Se for 25 ou mais: "Sobrepeso"
+function calcularIMC(peso, altura) {
+     let IMC = peso/(altura * altura);
+
+    if (IMC < 18.5) {
+        return 'Abaixo do peso';
+    } else if (IMC >= 18.5 && IMC <= 24.9) {
+        return 'Peso normal';
+    } else {
+        return 'Sobrepeso';
+    }
+}
+
+console.log(calcularIMC(44.8 , 1.55));
+
+//45. Conversor de Temperatura:Crie uma função que recebe uma temperatura em Celsius (C) e retorna a temperatura convertida em Fahrenheit (F).Fórmula: F = (C * 1.8) + 32;
+
+function convertorDeTemperatura(Celsius) {
+    return (Celsius * 1.8) + 32;
+}
+
+console.log(convertorDeTemperatura(20));
+
+//46. Calculadora de Desconto: Crie uma função que recebe o valor de um produto e a porcentagem de desconto (ex: 10 para 10%). A função deve retornar o novo valor com o desconto aplicado.
+//47. Sistema de Notas: Crie uma função que recebe uma nota de 0 a 10 e retorna: "Aprovado" se a nota for 7 ou maior. "Recuperação" se a nota for entre 5 e 6.9. "Reprovado" se a nota for menor que 5. Dica para a questão 46: Para calcular o desconto, você pode fazer: valor - (valor * (desconto / 100)).
